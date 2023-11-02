b94a83ebb3cf1dab1dc87910d98843ebc7fc94a9
adhamhassan99 <49523561+adhamhassan99@users.noreply.github.com>
2023-11-02 17:27:03 +0200

    basic trials for auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import { supabase } from "./supabaseClient";
 
 function App() {
   const [signIn] = useSignInWithOAuth();
+  supabase.auth.onAuthStateChange((e, s) => console.log(e, s));
   return (
     <div className="">
       <button onClick={() => signIn({ provider: "discord" })}>
         sign with otp
       </button>
+
+      <button onClick={() => supabase.auth.signOut()}>sign out</button>
     </div>
   );
 }
